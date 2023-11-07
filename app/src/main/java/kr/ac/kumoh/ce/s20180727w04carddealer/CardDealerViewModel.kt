39c1006f93f4275af07b8c1265986a40fdcb906e
SeungMoYu <smy0917@naver.com>
2023-11-07 22:17:21 +0900

    포커 족보 식별 및 10000회 시뮬레이션 추가

--- app/src/main/java/kr/ac/kumoh/ce/s20180727w04carddealer/CardDealerViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180727w04carddealer/CardDealerViewModel.kt
@@ -7,8 +7,11 @@ import kotlin.random.Random
 
 class CardDealerViewModel : ViewModel() {
     private var _cards = MutableLiveData<IntArray>(IntArray(5) { -1 })
+    private var _handRank: MutableLiveData<String> = MutableLiveData()
     val cards: LiveData<IntArray>
         get() = _cards
+    val handRank: LiveData<String>
+        get() = _handRank
     fun shuffle() {
         var num = 0
         val newCards = IntArray(5) { -1 }
@@ -20,10 +23,86 @@ class CardDealerViewModel : ViewModel() {
             } while (newCards.contains(num))
             newCards[i] = num
         }
-
-        // 정렬
         newCards.sort()
-
         _cards.value = newCards
     }
+
+    fun determineHandRank() {
+        _handRank.value = when {
+            isRoyalStraightFlush() ->  "로얄 스트레이트 플러쉬"
+            isBackStraightFlush() -> "백스트레이트 플러쉬"
+            isStraightFlush() -> "스트레이트 플러쉬"
+            isFourCards() -> "포카드"
+            isFullHouse() -> "풀하우스"
+            isFlush() -> "플러시"
+            isMountain() -> "마운틴"
+            isBackStraight() -> "백스트레이트"
+            isStraight() -> "스트레이트"
+            isTriple() -> "트리플"
+            isTwoPair() -> "투페어"
+            isPair() -> "원페어"
+            else -> "탑"
+        }
+    }
+
+    private fun isPair(): Boolean {
+        val numbers = _cards.value?.map { it%13 }?.toList()?.sorted()
+        return numbers?.toSet()?.size == 4
+    }
+    private fun isTwoPair(): Boolean {
+        val numbers = _cards.value?.map { it%13 }?.toList()?.sorted()
+        return numbers?.toSet()?.size == 3
+    }
+    private fun isTriple(): Boolean {
+        val numbers = _cards.value?.map { it%13 }?.toList()?.sorted()
+        for (i in numbers!!) {
+            if (numbers.count { it == i } == 3) return true
+        }
+        return false
+    }
+    private fun isFourCards(): Boolean {
+        val numbers = _cards.value?.map { it%13 }?.toList()?.sorted()
+        for (i in numbers!!) {
+            if (numbers.count { it == i } == 4) return true
+        }
+        return false
+    }
+    private fun isBackStraight(): Boolean {
+        val numbers = _cards.value?.map { it%13 }?.toList()?.sorted()
+        return numbers == intArrayOf(0, 1, 2, 3, 4).toList()
+    }
+    private fun isFlush(): Boolean {
+        val numbers = _cards.value!!.map { it/13 }.toList().sorted()
+        return numbers.all { it == numbers[0] }
+    }
+    private fun isMountain(): Boolean {
+        val numbers = _cards.value?.map { it%13 }?.toList()?.sorted()
+        return numbers == intArrayOf(0, 9, 10, 11, 12).toList()
+    }
+    private fun isStraight(): Boolean {
+        val numbers = _cards.value?.map { it%13 }?.toList()?.sorted()
+        for (i in 0 until 4) {
+            if (numbers?.get(i)!!+1 != numbers[i+1]) return false
+        }
+        return true
+    }
+    private fun isFullHouse(): Boolean {
+        if (isTriple()) {
+            val numbers = _cards.value?.map { it%13 }?.toList()?.sorted()
+            if (numbers?.toSet()?.size == 2) {
+                return true
+            }
+        }
+        return false
+    }
+    private fun isStraightFlush(): Boolean {
+        return isFlush() && isStraight()
+    }
+    private fun isBackStraightFlush(): Boolean {
+        return isFlush() && isBackStraight()
+    }
+    private fun isRoyalStraightFlush(): Boolean {
+        return isFlush() && isMountain()
+    }
 }
+
--- app/src/main/java/kr/ac/kumoh/ce/s20180727w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180727w04carddealer/MainActivity.kt
@@ -2,6 +2,11 @@ package kr.ac.kumoh.ce.s20180727w04carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.s20180727w04carddealer.databinding.ActivityMainBinding
@@ -32,8 +37,66 @@ class MainActivity : AppCompatActivity() {
             main.card5.setImageResource(res[4])
         })
 
+        model.handRank.observe(this) {
+            main.rank.text = model.handRank.value
+        }
         main.btnShuffle.setOnClickListener {
             model.shuffle()
+            model.determineHandRank()
+        }
+
+        main.btnSimulator?.setOnClickListener {
+            val result = IntArray(13) { 0 }
+            var simulationsRun = 0
+            val handler = Handler(Looper.getMainLooper())
+
+            // 셔플과 족보 판별을 반복적으로 수행하는 Runnable 객체
+            val simulationRunnable = object : Runnable {
+                override fun run() {
+                    if (simulationsRun < 10000) {
+                        model.shuffle()
+                        model.determineHandRank()
+                        simulationsRun++
+                        handler.postDelayed(this, 1)
+                    } else {
+                        val resultText = result.withIndex()
+                            .joinToString(separator = "\n") { (index, count) ->
+                                "${getHandRankString(index)}: ${count}회  /  ${String.format("%.2f", count / 10000.0 * 100)}%"
+                            }
+
+                        AlertDialog.Builder(this@MainActivity).apply {
+                            setTitle("시뮬레이션 결과")
+                            setMessage(resultText)
+                            setPositiveButton("확인") { dialog, which -> dialog.dismiss() }
+                            show()
+                        }
+                    }
+                }
+            }
+
+            val handRankObserver = Observer<String> { handRank ->
+                when (handRank) {
+                    "로얄 스트레이트 플러쉬" -> result[0]++
+                    "백스트레이트 플러쉬" -> result[1]++
+                    "스트레이트 플러쉬" -> result[2]++
+                    "포카드" -> result[3]++
+                    "풀하우스" -> result[4]++
+                    "플러시" -> result[5]++
+                    "마운틴" -> result[6]++
+                    "백스트레이트" -> result[7]++
+                    "스트레이트" -> result[8]++
+                    "트리플" -> result[9]++
+                    "투페어" -> result[10]++
+                    "원페어" -> result[11]++
+                    "탑" -> result[12]++
+                    else -> ""
+                }
+            }
+            // Observer 등록
+            model.handRank.observe(this, handRankObserver)
+
+            // 최초의 시뮬레이션 시작
+            handler.post(simulationRunnable)
         }
     }
 
@@ -68,4 +131,25 @@ class MainActivity : AppCompatActivity() {
             return "c_red_joker"
         return "c_${number}_of_${shape}"
     }
+
+    private fun getHandRankString(index: Int): String {
+        return when (index) {
+            0 -> "로얄 스트레이트 플러쉬"
+            1 -> "백스트레이트 플러쉬"
+            2 -> "스트레이트 플러쉬"
+            3 -> "포카드"
+            4 -> "풀하우스"
+            5 -> "플러시"
+            6 -> "마운틴"
+            7 -> "백스트레이트"
+            8 -> "스트레이트"
+            9 -> "트리플"
+            10 -> "투페어"
+            11 -> "원페어"
+            12 -> "탑"
+            else -> "알 수 없음"
+        }
+    }
+
 }
+
